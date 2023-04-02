36998ecd447ead694046c502e649e90e7322c295
Administrator <Administrator@PC-202303101843>
2023-04-02 17:22:19 +0900

    초기 커밋.

---
+++ dojang/FileName.c
@@ -0,0 +1,58 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+#define MAX_STACK_SIZE 10
+
+typedef struct {
+    int top;
+    int* data;
+} Stack;
+
+void init_stack(Stack* s) {
+    s->top = -1;
+    s->data = (int*)malloc(sizeof(int) * MAX_STACK_SIZE);
+}
+
+int is_full(Stack* s) {
+    return (s->top == MAX_STACK_SIZE - 1);
+}
+
+int is_empty(Stack* s) {
+    return (s->top == -1);
+}
+
+void push(Stack* s, int item) {
+    if (is_full(s)) {
+        printf("Stack Full\n");
+        return;
+    }
+    s->data[++(s->top)] = item;
+}
+
+int pop(Stack* s) {
+    if (is_empty(s)) {
+        printf("Stack Empty\n");
+        return -1;
+    }
+    return s->data[(s->top)--];
+}
+
+int main() {
+    Stack s;
+    init_stack(&s);
+    srand(time(NULL)); 
+    for (int i = 0; i < 30; i++) {
+        int rand_num = rand() % 100 + 1;
+        if (rand_num % 2 == 0) {
+            push(&s, rand_num);
+            printf("Push %d\n", rand_num);
+        }
+        else {
+            int popped_num = pop(&s);
+            printf("Pop %d\n", popped_num);
+        }
+    }
+    free(s.data);
+    return 0;
+}
